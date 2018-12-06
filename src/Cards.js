57721ee2b629af4047fd648579de229dd531a663
Harlan Evans <harlanevans5@gmail.com>
2018-12-05 19:03:15 -0700

    styling works

--- src/Cards.js
+++ src/Cards.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { Card, } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 
 const Cards = ({ varCards, }) => (
   // Semantic Card 
   <Card.Group>
     {
       varCards.map(v => (
-          <Card key={v.id}>
-            {v.frontQuestion}
-          </Card>
+        <Card key={v.id}>
+          <Card.Content>
+            <Card.Header>
+              Question
+            </Card.Header>
+            <Card.Description>
+              {v.frontQuestion}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+              <Button basic color='blue'>
+                Edit
+          </Button>
+              <Button basic color='red'>
+                Delete
+          </Button>
+            </div>
+          </Card.Content>
+        </Card>
       ))
     }
   </Card.Group>
